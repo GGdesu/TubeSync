95025b44fb54ce94aa3111f08979a3c51eb402a6
Gabriel Gomes <gabrielgomesdesantana@gmail.com>
2023-04-18 17:48:20 -0300

    modificacoes para o deploy na branch

--- server/app.js
+++ server/app.js
@@ -65,6 +65,18 @@ const roomNSP = io.of("/room");
   
 })
 
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+          if(err) {
+              res.status(500).send(err)
+          }
+      });
+  })
+}
+
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 
