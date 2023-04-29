bf1b2f35ff316094d6557fb9884ef80e038b6409
GGdesu <gabrielgomesdesantana@gmail.com>
2023-04-29 19:20:06 -0300

    apagando coisas inuteis no index.js(antigo app.js)

--- server/index.js
+++ server/index.js
@@ -70,17 +70,7 @@ roomNSP.on("connection", (socket) => {
 
 })
 
-if (process.env.NODE_ENV === "production") {
-  const path = require("path");
-  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'), function (err) {
-      if (err) {
-        res.status(500).send(err)
-      }
-    });
-  })
-}
+
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
